1f84be067c061cf6f1611cef4f9d8e73dce547c6
Kraiba Semakula <kraiba@Kraibas-MacBook-Pro.local>
2015-11-05 17:02:33 +0300

    mytet

--- gosacco/settings.py
+++ gosacco/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -95,8 +95,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5000',
